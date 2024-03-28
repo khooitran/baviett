9dc28bfd210a3f80087525aae7e26be70560c40e
khooi.dev <khooi.dev@gmail.com>
2024-03-29 00:02:48 +0700

    fixed directory

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+  root: 'src/',
+  publicDir: '../static/',
+  base: './',
+  server: {
+    host: true, // Open to local network and display URL
+    open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact/index.html'),
+        director: resolve(__dirname, 'director/index.html'),
+        editor: resolve(__dirname, 'editor/index.html'),
+        vfx: resolve(__dirname, 'vfx/index.html'),
+      },
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
-    },
-}
+    outDir: '../dist', // Output in the dist/ folder
+    emptyOutDir: true, // Empty the folder first
+    sourcemap: true, // Add sourcemap
+  },
+};
